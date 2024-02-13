13b410213e1cd33fbe56e3a460b8df8f91145c38
codehunterzahid <codehuntingwithzahid@gmail.com>
2024-02-13 21:57:13 +0500

    formatted

--- js/loginForm.js
+++ js/loginForm.js
@@ -1,13 +1,13 @@
 const password = document.querySelector("#password");
 const openEye = document.querySelector("#openEye");
 
-openEye.addEventListener("click", () =>{
+openEye.addEventListener("click", () => {
 
-    if(password.type === "password"){
+    if (password.type === "password") {
         password.type = "text";
         openEye.classList.remove("fa-eye-slash");
         openEye.classList.add("fa-eye");
-    } else{
+    } else {
         password.type = "password";
         openEye.classList.remove("fa-eye");
         openEye.classList.add("fa-eye-slash");
