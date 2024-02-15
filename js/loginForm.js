096aa8ce8a743b4c467fb6ff794fe71a3dbee4a3
codehunterzahid <codehuntingwithzahid@gmail.com>
2024-02-15 21:51:55 +0500

    password strength function is created

--- js/loginForm.js
+++ js/loginForm.js
@@ -1,5 +1,29 @@
 const password = document.querySelector("#password");
 const openEye = document.querySelector("#openEye");
+const msg = document.querySelector(".msg");
+
+password.addEventListener("input", ()=>{
+    if(password.value.length >0){
+        msg.style.display = "block";
+    } else{
+        msg.style.display = "none";
+        password.style.borderBottom = "2px solid #2e2e2e";
+    }
+
+    if(password.value.length <4){
+        msg.innerHTML = "Your Password is Weak";
+        password.style.borderBottom = "2px solid red";
+        msg.style.color = "red";
+    } else if(password.value.length >4 && password.value.length <8){
+        msg.innerHTML = "Your Password is Medium";
+        password.style.borderBottom = "2px solid orange";
+        msg.style.color = "orange";
+    } else if(password.value.length >8){
+        msg.innerHTML = "Your Password is Strong";
+        password.style.borderBottom = "2px solid green";
+        msg.style.color = "green";
+    }
+})
 
 openEye.addEventListener("click", () => {
 
